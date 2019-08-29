cf164d4461efd282dcdb5eb26fb3d4ca861cfa68
Gabriel Lima <gabriel.lima1000@gmail.com>
2019-08-29 15:44:49 -0400

    descobrir a porta heroku

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/',(req,res)=>{
-  res.send('Teste Hospedagem Heroku');
+  res.send(`Teste Hospedagem Heroku ${port}`);
 })
 
 app.listen(port);
