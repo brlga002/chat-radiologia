ca9f771057f925eafa8be2e97a450f8b03e19546
brlga002 <gabriel.lima1000@gmail.com>
2019-09-02 14:28:12 -0400

    seraparação da logica

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
-const delay = require('delay')
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
+const server = require('http').Server(app);
+
+require('./src/io')(server);
 
 const handlebars = require('express-handlebars');
 const path = require('path');
@@ -18,42 +18,7 @@ app.get('/', function(req, res) {
   });
 });
 
-io.on('connection', (socket) => {
-  console.log('New user connected ' + socket.id);
-  socket.on('welcome', () => {    
-    (async () => {
-      await delay(1000);
-      io.to(`${socket.id}`).emit('new_message', { message: "Olá", username: "Procurando..." }); 
-      await delay(1000);     
-      io.to(`${socket.id}`).emit('new_message', { message: "Seja bem vindo ao atendimento do CRTR 19ª Região", username: "Procurando..." });
-      await delay(1000);
-      io.to(`${socket.id}`).emit('new_message', { message: "Você pode me informar seu nome ou CPF?", username: "Procurando..." });     
-      await delay(5000);
-      io.to(`${socket.id}`).emit('new_message', { message: "So um minuto, Estou procurando um atendente para você", username: "Procurando..." });     
-    })();
-  })
-  
-  // default username
-  socket.username = 'Anonymous';
-  
-  // listen on change_username
-  socket.on('change_username', (data) => {
-    if ( data.username != ""){
-      socket.username = data.username;
-    }  
-  });
-  
-  // listen on new_message
-  socket.on('new_message', (data) => {
-    // broadcast the new message    
-    io.sockets.emit('new_message', { message: data.message, username: socket.username });
-  });
-  
-  socket.on('disconnect', () => {    
-    console.log("disconnect")
-  })
-  
-});
+
 
 server.listen(port);
 console.log('Server online na porta: ' + port);
