892f5e3d11536078d2b743e62b9dd1368c30f6af
brlga002 <gabriel.lima1000@gmail.com>
2020-07-30 22:53:38 -0400

    teste de rota na rota /port

--- src/index.js
+++ src/index.js
@@ -1,19 +1,35 @@
 const express = require('express');
+const jsonServer = require('json-server');
+const path = require('path');
 const cors = require('cors');
+const request = require('supertest');
 
 const app = express();
 const server = require('http').Server(app);
-const path = require('path');
 
+const serveJson = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 5000;
+
+serveJson.use(middlewares);
+serveJson.use(router);
 
 app.use(cors());
 app.use(express.json());
 app.use('/', express.static(path.resolve(__dirname, '..', 'client')));
 
 app.get('/port', async (req, res) => {
-  res.send({ port });
+  //res.send({ port });
+  //const response = await request(app).get('/posts/1?_expand=menu');
+  const response = await request(app).get(
+    '/posts/?_expand=menu&botName=baixaRegistro'
+  );
+  const messages = JSON.parse(response.text);
+  res.send(messages[0]);
 });
+
+app.use(serveJson);
 
 require('./services/socket_rules')(server);
 
